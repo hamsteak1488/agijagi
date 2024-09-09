a917c13040c6f20fc81d5ec136b5bc3bf409c18e
권용진 <dydwls9703@naver.com>
2024-09-09 15:00:52 +0900

    be/chore: 백엔드 서버 환경 설정
    spring server init
    
    #8

---
+++ backend/agijagi/src/test/java/com/password926/agijagi/AgijagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.password926.agijagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgijagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
