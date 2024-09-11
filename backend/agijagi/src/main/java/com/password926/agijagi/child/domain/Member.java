a3c43f23774fa2a5628b033f579063e9ae17cb5b
권용진 <dydwls9703@naver.com>
2024-09-11 17:55:45 +0900

    be/feat: 임시 Member.class 작성
    #13

---
+++ backend/agijagi/src/main/java/com/password926/agijagi/child/domain/Member.java
@@ -0,0 +1,11 @@
+package com.password926.agijagi.child.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private long id;
+}
