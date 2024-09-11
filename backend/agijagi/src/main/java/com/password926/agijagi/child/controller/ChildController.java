54af0b1480ef501690ca0c03f394b1f129bb7632
권용진 <dydwls9703@naver.com>
2024-09-11 17:52:16 +0900

    be/feat: ChildController 작성
    #9

--- backend/agijagi/src/main/java/com/password926/agijagi/child/controller/ChildController.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/child/controller/ChildController.java
@@ -1,10 +1,13 @@
 package com.password926.agijagi.child.controller;
 
+import com.password926.agijagi.child.domain.ChildDetail;
 import com.password926.agijagi.child.service.ChildService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,7 +16,7 @@ public class ChildController {
     private final ChildService childService;
 
     @GetMapping("/child")
-    public ResponseEntity<Void> findChildren() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<ChildDetail>> readChildDetail(long memberId) {
+        return ResponseEntity.ok().body(childService.readChildDetail(memberId));
     }
 }
