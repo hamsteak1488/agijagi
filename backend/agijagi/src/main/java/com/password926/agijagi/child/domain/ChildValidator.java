acaec5019d42b6e77c5e6b7e24ffd8150c18a460
sarmsoo <rnjsdydwls97@gmail.com>
2024-09-16 18:46:31 +0900

    be/feat: ChildValidator 구현, 로직 수정
    #28

---
+++ backend/agijagi/src/main/java/com/password926/agijagi/child/domain/ChildValidator.java
@@ -0,0 +1,20 @@
+package com.password926.agijagi.child.domain;
+
+import com.password926.agijagi.child.controller.ChildErrorCode;
+import com.password926.agijagi.common.errors.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class ChildValidator {
+
+    private final MemberChildReader memberChildReader;
+
+    public void validateWriterRole(long memberId, long childId) {
+        MemberChild memberChild = memberChildReader.readByMemberAndChild(memberId, childId);
+        if (!"WRITE".equals(memberChild.getRole())) {
+            throw new RestApiException(ChildErrorCode.UNAUTHORIZED);
+        }
+    }
+}
