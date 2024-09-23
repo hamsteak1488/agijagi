595b5ba3f0430ce512eea9d6c29b39a15b071c94
권용진 <dydwls9703@naver.com>
2024-09-23 16:08:24 +0900

    be/feat: 아이 이미지 수정, 삭제 기능 구현
    #44

--- backend/agijagi/src/main/java/com/password926/agijagi/child/domain/ChildImageRemover.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/child/domain/ChildImageRemover.java
@@ -2,16 +2,18 @@ package com.password926.agijagi.child.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
-public class ChildUpdater {
+public class ChildImageRemover {
 
     private final ChildValidator childValidator;
     private final ChildReader childReader;
 
-    public void update(long memberId, long childId, ChildContent childContent) {
+    @Transactional
+    public void remove(long memberId, long childId) {
         childValidator.validateWriterRole(memberId, childId);
-        childReader.read(childId).update(childContent);
+        childReader.read(childId).updateImage(null);
     }
 }
