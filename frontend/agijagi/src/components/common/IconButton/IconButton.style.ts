07c35b60879ed296a6d677656672e0c045e79076
kimdw524 <kimdw524@naver.com>
2024-09-23 17:35:48 +0900

    fe/fix: IconButton color 연하게 수정
    #000 -> greyScale.900 으로 수정함
    
    #47

--- frontend/agijagi/src/components/common/IconButton/IconButton.style.ts
+++ frontend/agijagi/src/components/common/IconButton/IconButton.style.ts
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
+import theme from '../../../styles/theme';
+
 export const Container = styled.div`
   width: 2rem;
   height: 2rem;
+  color: ${theme.color.greyScale[900]};
 `;
