da40d92c813fe1419a30ee1c3a72177ccdd700c3
권용진 <dydwls9703@naver.com>
2024-09-24 00:57:37 +0900

    be/feat: Schedule @Builder 추가
    #49

--- backend/agijagi/src/main/java/com/password926/agijagi/schedule/domain/Schedule.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/schedule/domain/Schedule.java
@@ -3,12 +3,14 @@ package com.password926.agijagi.schedule.domain;
 import com.password926.agijagi.child.domain.Child;
 import com.password926.agijagi.member.domain.Member;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Schedule {
 
