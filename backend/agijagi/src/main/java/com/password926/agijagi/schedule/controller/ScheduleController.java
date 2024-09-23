08988fd995795e128c5527271853f348ba905237
권용진 <dydwls9703@naver.com>
2024-09-24 00:16:43 +0900

    be/feat: 일정 조회 기능 구현
    #46

--- backend/agijagi/src/main/java/com/password926/agijagi/schedule/controller/ScheduleController.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/schedule/controller/ScheduleController.java
@@ -1,13 +1,33 @@
 package com.password926.agijagi.schedule.controller;
 
+import com.password926.agijagi.schedule.controller.dto.ScheduleDtoConverter;
+import com.password926.agijagi.schedule.controller.dto.response.ReadScheduleResponse;
+import com.password926.agijagi.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RequestMapping("/schedules")
 @RequiredArgsConstructor
 @RestController
 public class ScheduleController {
 
+    private final ScheduleService scheduleService;
 
+    @GetMapping
+    public ResponseEntity<List<ReadScheduleResponse>> readSchedule(
+            long memberId,
+            @RequestParam long childId,
+            @RequestParam LocalDate startDate,
+            @RequestParam LocalDate endDate
+    ) {
+        return ResponseEntity.ok()
+                .body(ScheduleDtoConverter.convert(scheduleService.readSchedule(memberId, childId, startDate, endDate)));
+    }
 }
