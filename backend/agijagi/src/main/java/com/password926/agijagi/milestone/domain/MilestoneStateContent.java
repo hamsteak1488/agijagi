ebf1517adc5196f8a8908bd5ae12d9e607b66540
권용진 <dydwls9703@naver.com>
2024-09-24 14:08:02 +0900

    be/feat: 마일스톤 수정 기능 구현
    #51

---
+++ backend/agijagi/src/main/java/com/password926/agijagi/milestone/domain/MilestoneStateContent.java
@@ -0,0 +1,13 @@
+package com.password926.agijagi.milestone.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MilestoneStateContent {
+
+    private long id;
+
+    private int currentAmount;
+}
--- backend/agijagi/src/main/java/com/password926/agijagi/milestone/service/MilestoneService.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/milestone/service/MilestoneService.java
@@ -1,7 +1,9 @@
 package com.password926.agijagi.milestone.service;
 
+import com.password926.agijagi.milestone.domain.MilestoneStateContent;
 import com.password926.agijagi.milestone.domain.MilestoneStateDetail;
 import com.password926.agijagi.milestone.domain.MilestoneStateDetailReader;
+import com.password926.agijagi.milestone.domain.MilestoneStateUpdater;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,17 @@ import java.util.List;
 public class MilestoneService {
 
     private final MilestoneStateDetailReader milestoneStateDetailReader;
+    private final MilestoneStateUpdater milestoneStateUpdater;
 
     public List<MilestoneStateDetail> readMilestone(long memberId, long childId, int month) {
         return milestoneStateDetailReader.read(memberId, childId, month);
+    }
+
+    public void updateMilestone(
+            long memberId,
+            long childId,
+            List<MilestoneStateContent> milestoneStateContents
+    ) {
+        milestoneStateUpdater.update(memberId, childId, milestoneStateContents);
     }
 }
