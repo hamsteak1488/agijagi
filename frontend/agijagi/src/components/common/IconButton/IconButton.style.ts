84785ea1dbf74b7772c8a1e78c6ded829e65c264
kimdw524 <kimdw524@naver.com>
2024-09-24 10:36:14 +0900

    fe/feat: IconButton animation 추가
    interaction animation 추가
    
    #47

--- frontend/agijagi/src/components/common/IconButton/IconButton.style.ts
+++ frontend/agijagi/src/components/common/IconButton/IconButton.style.ts
@@ -6,4 +6,11 @@ export const Container = styled.div`
   width: 2rem;
   height: 2rem;
   color: ${theme.color.greyScale[900]};
+  transition: all 0.2s ease;
+  cursor: pointer;
+
+  :active {
+    color: ${theme.color.greyScale[800]};
+    transform: scale(0.9);
+  }
 `;
