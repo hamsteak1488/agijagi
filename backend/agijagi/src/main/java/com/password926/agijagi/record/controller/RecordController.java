50d28655e67a2e5e3b8743cb259b8f13ec716b0d
권용진 <dydwls9703@naver.com>
2024-09-25 17:06:02 +0900

    be/refactor: recordController end point 수정
    #60

--- backend/agijagi/src/main/java/com/password926/agijagi/record/controller/RecordController.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/record/controller/RecordController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RequestMapping("/records")
+@RequestMapping("/children/records")
 @RequiredArgsConstructor
 @RestController
 public class RecordController {
