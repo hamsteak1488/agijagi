00d7e02a49377c1764fdc2ae57c94af4b091f435
kimdw524 <kimdw524@naver.com>
2024-09-26 16:04:26 +0900

    fe/feat: 글쓰기 modal 추가
    #72

--- frontend/agijagi/src/pages/BoardPage/WritePage/style.ts
+++ frontend/agijagi/src/pages/BoardPage/WritePage/style.ts
@@ -5,7 +5,9 @@ import theme from '../../../styles/theme';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100vw;
   height: 100vh;
+  background-color: ${theme.color.primary[100]};
 `;
 
 export const Main = styled.main`
