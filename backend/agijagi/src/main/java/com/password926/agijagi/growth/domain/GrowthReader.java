56ae61477fe4400022cd7d3cc97ce7c7f1a35f60
권용진 <dydwls9703@naver.com>
2024-09-27 17:51:53 +0900

    be/wip: 중간 저장
    #83

---
+++ backend/agijagi/src/main/java/com/password926/agijagi/growth/domain/GrowthReader.java
@@ -0,0 +1,11 @@
+package com.password926.agijagi.growth.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class GrowthReader {
+
+
+}
---
+++ backend/agijagi/src/main/java/com/password926/agijagi/growth/infrastructure/GrowthRepository.java
@@ -0,0 +1,8 @@
+package com.password926.agijagi.growth.infrastructure;
+
+import com.password926.agijagi.growth.domain.Growth;
+import org.springframework.data.repository.Repository;
+
+public interface GrowthRepository extends Repository<Growth, Long> {
+
+}
