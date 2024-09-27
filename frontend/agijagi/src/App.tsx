aa468edbcf925f545cc7931625b4e465b95df835
kimdw524 <kimdw524@naver.com>
2024-09-27 13:53:16 +0900

    fe/feat: queryClient
    #74

--- frontend/agijagi/src/App.tsx
+++ frontend/agijagi/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import WritingDiary from './pages/WritingDiary';
-import ActivityRecordPage from './pages/ActivityRecordPage';
+import RecordPage from './pages/RecordPage';
 import SchedulePage from './pages/SchedulePage';
 import BabyMain from './pages/BabyMain';
 import Login from './pages/Login';
@@ -13,24 +13,36 @@ import GlobalStyle from './styles/GlobalStyle';
 
 import { ModalProvider } from './hooks/useModal';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+      },
+    },
+  });
+
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <ModalProvider>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/activity-record" element={<ActivityRecordPage />} />
-            <Route path="/baby" element={<BabyMain />} />
-            <Route path="/baby/writing" element={<WritingDiary />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/board" element={<BoardPage />} />
-            <Route path="/board/write" element={<WriteArticlePage />} />
-          </Routes>
-        </ModalProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ModalProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/record" element={<RecordPage />} />
+              <Route path="/baby" element={<BabyMain />} />
+              <Route path="/baby/writing" element={<WritingDiary />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/schedule" element={<SchedulePage />} />
+              <Route path="/board" element={<BoardPage />} />
+              <Route path="/board/write" element={<WriteArticlePage />} />
+            </Routes>
+          </ModalProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 }
