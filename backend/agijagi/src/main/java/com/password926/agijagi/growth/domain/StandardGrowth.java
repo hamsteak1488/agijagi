55d5e4a5acea5b6c4e5028ff3ef3d699cbb44dff
권용진 <dydwls9703@naver.com>
2024-09-28 22:31:21 +0900

    be/feat: 성장 조회 기능 구현
    #83

--- backend/agijagi/src/main/java/com/password926/agijagi/growth/domain/StandardGrowth.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/growth/domain/StandardGrowth.java
@@ -1,17 +1,16 @@
 package com.password926.agijagi.growth.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class StandardGrowth {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private double standardWeight;
-
-    private double standardHeight;
 
     private int month;
 }
