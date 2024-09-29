14947a39595baadb04277eade055cc9ec6e815a1
권용진 <dydwls9703@naver.com>
2024-09-29 15:25:39 +0900

    be/feat: 표준 성장에 Gender 필드 추가
    #76

--- backend/agijagi/src/main/java/com/password926/agijagi/growth/domain/StandardGrowth.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/growth/domain/StandardGrowth.java
@@ -1,5 +1,7 @@
 package com.password926.agijagi.growth.domain;
 
+import com.password926.agijagi.child.domain.Gender;
+import com.password926.agijagi.child.domain.GenderConverter;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -8,9 +10,16 @@ import lombok.Getter;
 public class StandardGrowth {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(nullable = false)
     private double standardWeight;
 
+    @Column(nullable = false)
     private int month;
+
+    @Convert(converter = GenderConverter.class)
+    @Column(nullable = false)
+    private Gender gender;
 }
