0f9cfc3e5ea6e7278091f1f8a8a696ccbd92d60a
권용진 <dydwls9703@naver.com>
2024-09-30 20:44:29 +0900

    be/fix: milestone controller layer 수정
    #94

--- backend/agijagi/src/main/java/com/password926/agijagi/milestone/controller/dto/MilestoneDtoConverter.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/milestone/controller/dto/MilestoneDtoConverter.java
@@ -1,7 +1,6 @@
 package com.password926.agijagi.milestone.controller.dto;
 
 import com.password926.agijagi.milestone.controller.dto.response.ReadMilestoneResponse;
-import com.password926.agijagi.milestone.controller.dto.response.ReadMilestoneResponseContent;
 import com.password926.agijagi.milestone.domain.MilestoneStateDetail;
 
 import java.util.ArrayList;
@@ -11,19 +10,19 @@ import java.util.List;
 public class MilestoneDtoConverter {
 
     public static List<ReadMilestoneResponse> convert(List<MilestoneStateDetail> milestones) {
-        LinkedHashMap<String, List<ReadMilestoneResponseContent>> map = new LinkedHashMap<>();
+        LinkedHashMap<String, List<ReadMilestoneResponse.ResponseContent>> map = new LinkedHashMap<>();
 
         for (MilestoneStateDetail ms : milestones) {
             String title = ms.getTitle();
             if (!map.containsKey(title)) {
                 map.put(title, new ArrayList<>());
             }
-            map.get(title).add(ReadMilestoneResponseContent.from(ms));
+            map.get(title).add(ReadMilestoneResponse.ResponseContent.from(ms));
         }
 
         List<ReadMilestoneResponse> response = new ArrayList<>();
         for (String title : map.keySet()) {
-            List<ReadMilestoneResponseContent> value = map.get(title);
+            List<ReadMilestoneResponse.ResponseContent> value = map.get(title);
             response.add(new ReadMilestoneResponse(title, value));
         }
 
