8b1a4f28a9fb618a140e0c5dedc32c74dfb8fc03
권용진 <dydwls9703@naver.com>
2024-10-04 10:43:42 +0900

    be/feat: 비동기 이미지 생성 기능 구현
    #109

--- backend/agijagi/src/main/java/com/password926/agijagi/AgijagiApplication.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/AgijagiApplication.java
@@ -2,7 +2,9 @@ package com.password926.agijagi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class AgijagiApplication {
 
