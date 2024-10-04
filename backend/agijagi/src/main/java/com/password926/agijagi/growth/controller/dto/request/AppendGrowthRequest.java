145538733dfada0715b526dc6a25a8238ea95807
권용진 <dydwls9703@naver.com>
2024-10-04 13:51:45 +0900

    be/fix: 음수 개월은 등록 못하도록 수정
    #115

--- backend/agijagi/src/main/java/com/password926/agijagi/growth/controller/dto/request/AppendGrowthRequest.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/growth/controller/dto/request/AppendGrowthRequest.java
@@ -1,6 +1,7 @@
 package com.password926.agijagi.growth.controller.dto.request;
 
 import com.password926.agijagi.growth.domain.GrowthContent;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -13,6 +14,7 @@ public class AppendGrowthRequest {
     @NotNull
     private double height;
 
+    @Min(0)
     private int month;
 
     public GrowthContent toContent() {
