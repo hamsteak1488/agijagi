8017f6a07aa5f2ae5dd91e771d7ce36767cc6e84
신재민 <woals1488@naver.com>
2024-10-07 16:27:05 +0900

    be/fix: 게시글 조회했을 때, 미디어 Url 순서가 지켜지도록 수정
    #121

--- backend/agijagi/src/main/java/com/password926/agijagi/media/infrastructure/MediaRepository.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/media/infrastructure/MediaRepository.java
@@ -1,10 +1,8 @@
 package com.password926.agijagi.media.infrastructure;
 
 import com.password926.agijagi.media.domain.Media;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,6 +10,4 @@ public interface MediaRepository extends Repository<Media, Long> {
     Media save(Media media);
     Optional<Media> findById(UUID id);
 
-    @Query("SELECT m.url FROM Media as m WHERE m.id IN :ids")
-    List<String> findUrlByIdIn(List<UUID> ids);
 }
--- backend/agijagi/src/main/java/com/password926/agijagi/post/domain/PostMediaReader.java
+++ backend/agijagi/src/main/java/com/password926/agijagi/post/domain/PostMediaReader.java
@@ -1,27 +1,26 @@
 package com.password926.agijagi.post.domain;
 
 import com.password926.agijagi.media.domain.Media;
-import com.password926.agijagi.media.domain.MediaReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class PostMediaReader {
-
     private final PostReader postReader;
-    private final MediaReader mediaReader;
 
     @Transactional(readOnly = true)
     public List<String> readPostMediaUrls(long postId) {
         Post post = postReader.read(postId);
 
-        return mediaReader.getMediaUrls(post.getPostMediaList().stream()
+        return post.getPostMediaList().stream()
+                .sorted(Comparator.comparingInt(PostMedia::getMediaOrder))
                 .map(PostMedia::getMedia)
-                .map(Media::getId)
-                .toList());
+                .map(Media::getUrl)
+                .toList();
     }
 }
